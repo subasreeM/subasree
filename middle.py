fa80f1e8482bc156b81494b2166b588662bec31d
subasreeM <50616288+subasreeM@users.noreply.github.com>
2019-06-22 19:10:19 +0530

    Update middle.py

--- middle.py
+++ middle.py
@@ -1,8 +1,8 @@
-d=input()
+d=list(input())
 if(len(d)%2==0):
 	d[int(len(d)/2)]="*"
 	d[int(len(d)/2)-1]="*"
 else:
 	d[int(len(d)/2)]="*"
 for i in range(0,len(d)):
-	d[int(len(d)/2)]="*"
+	print(d[i],end="")
